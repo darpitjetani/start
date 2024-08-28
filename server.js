858662f1c6bf5a422bf850cd808af74d55fae077
darpit jetani <108603686+darpitjetani@users.noreply.github.com>
2024-08-28 18:11:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,14 +33,8 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
+  origin: allowedOrigins,
+  credentials: true, // જો માન્યતા (authentication) જરૂરી હોય તો
 };
 
 app.use(cors(corsOptions));
