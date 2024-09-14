656282754445b04e817160a5a5ac4adbc903087a
darpit jetani <108603686+darpitjetani@users.noreply.github.com>
2024-09-14 11:22:05 +0530

    Update authRoute.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -65,5 +65,15 @@ router.get('/user-count', async (req, res) => {
   }
 });
 
+  router.get('/api/referred-users/:referenceCode', async (req, res) => {
+    try {
+      const users = await User.find({ referenceCode: req.params.referenceCode });
+      res.json(users);
+    } catch (error) {
+      console.error('Error fetching referred users:', error);
+      res.status(500).json({ error: 'Server error' });
+    }
+  });
+
 
 module.exports = router;
