9870dbe0f25a8e6abdd4a728f29d935ca3281f19
darpit jetani <108603686+darpitjetani@users.noreply.github.com>
2024-09-23 11:08:00 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -209,9 +209,46 @@ const JWT = require("jsonwebtoken");
   }
 }; 
 
+const updateProfileController = async (req, res) => {
+  try {
+    const { firstname, middlename, lastname, address, aadhaar, pan, email, mobile, password } = req.body;
+    const user = await userModel.findById(req.user._id);
+    //password
+    if (password && password.length < 6) {
+      return res.json({ error: "Passsword is required and 6 character long" });
+    }
+    const hashedPassword = password ? await hashPassword(password) : undefined;
+    const updatedUser = await userModel.findByIdAndUpdate(
+      req.user._id,
+      {
+        firstname: firstname || user.firstname,
+        middlename: middlename || user.middlename,
+        lastname: lastname || user.lastname,
+        address: address || user.address,
+        aadhaar: aadhaar || user.aadhaar,
+        pan: pan || user.pan,
+        email: email || user.email,
+        mobile: mobile || user.mobile,
+        password: hashedPassword || user.password,
+      },
+      { new: true }
+    );
+    res.status(200).send({
+      success: true,
+      message: "Profile Updated Successfully",
+      updatedUser,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({
+      success: false,
+      message: "Error WHile Update profile",
+      error,
+    });
+  }
+};
 
 
-
-module.exports = { registerController, loginController, testController, forgotPasswordController, getAllUserController};
+module.exports = { registerController, loginController, testController, forgotPasswordController, getAllUserController, updateProfileController};
 
 
