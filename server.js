0e37172af6321497c00c13f3d54de2546a4651f2
darpit jetani <108603686+darpitjetani@users.noreply.github.com>
2024-09-23 17:51:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ app.use(bodyParser.json())
 app.use(express.static('public'));
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
+app.use('/Images', express.static(path.join(__dirname, 'public/Images')));
+app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/public', express.static('public'));
 
 
