82a56dcdc352be6b2aa8898f04dfe67307bf0ea1
darpit jetani <108603686+darpitjetani@users.noreply.github.com>
2024-09-24 15:12:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ connectDB();
 
 const app = express();
 
-app.use('/public', express.static('public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 
 app.use(bodyParser.json())
 app.use(express.static('public'));
